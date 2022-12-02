35c176545de29f85df2ea903e02a3821d1fe2963
A.M <andrej2000aa@gmail.com>
2022-12-02 11:19:45 +0100

    dodan novi ikone za vreme

--- index.py
+++ index.py
@@ -19,7 +19,7 @@ def home():
 
 def horoskopScrap():
     # Ovan
-    @app.route('/ovan')
+    @app.route('/ovan', methods=['GET', 'POST'])
     def ovan():
         url = 'http://www.horoskopius.com/dnevni-horoskop/ovan/'
         url_n = 'http://www.horoskopius.com/nedeljni-horoskop/ovan/'
